19d043c1b5481764d2aad019c98f4ed6f699e7ba
Nitin Bhatia <nitinbhatia@gmail.com>
2022-04-22 16:09:39 +0530

    udemy solidity using compiler 0.8

---
+++ deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+
+const { abi, evm } = require('./compile');
+
+provider = new HDWalletProvider(
+  'finger digital else chunk hen repair attitude leader story address join spray',
+'https://rinkeby.infura.io/v3/2155460f4f6145578d5d950175d4dca4'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+deploy();
